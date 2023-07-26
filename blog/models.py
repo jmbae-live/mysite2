b60deefcaca75267c188ad1ef385997b957340b0
JM Bae <jungman82@gmail.com>
2023-07-26 15:32:51 +0900

    get_absolute_url 추가
    포스트 목록 요소 수정

--- blog/models.py
+++ blog/models.py
@@ -25,3 +25,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('blog:post_detail', args=[self.id])
