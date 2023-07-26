626767dfd49e2067ab0aa9743f9184a5d83cfbec
JM Bae <jungman82@gmail.com>
2023-07-26 13:41:02 +0900

    기본 포스트 목록 페이지, 상세 페이지 추가

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post
 
 
 # Create your views here.
 def post_list(request):
-    pass
+    posts = Post.objects.filter(status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, post_id):
-    pass
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post/detail.html', {'post': post})
