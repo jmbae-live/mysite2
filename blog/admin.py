5e0076d1213cfb60ae0956dfdf0fe0bb58cd41fc
JM Bae <jungman82@gmail.com>
2023-07-26 11:38:06 +0900

    포스트 어드민 모델 작성

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from blog.models import Post
+
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'published', 'status']
