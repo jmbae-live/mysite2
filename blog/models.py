e751e5ee5b5a46acb934eef3daa566e711f3ac4b
JM Bae <jungman82@gmail.com>
2023-07-26 10:39:30 +0900

    포스트 모델 추가, 마이그레이션 수행

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(fields=['-published'])
+        ]
