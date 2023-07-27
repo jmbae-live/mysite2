9e1533a3c87e72297f0993ce09f263e12e72c983
JM Bae <jungman82@gmail.com>
2023-07-27 09:50:38 +0900

    뷰 함수, 템플릿 추가

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:post_id>', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share', views.post_share, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
+from blog.forms import EmailPostForm
 from blog.models import Post
 
 
@@ -21,3 +23,22 @@ def post_list(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{data['name']} 님이 {post.title}을(를) 추천합니다."
+            message = f"{post.title}을 {post_url}에서 읽어보세요.\n\n" \
+                      f"{data['name']}님의 의견: {data['comments']}"
+            send_mail(subject, message, 'jungman82@gmail.com', [data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
