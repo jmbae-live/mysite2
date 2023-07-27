55f9a011c07fb2817c5fe8c013b54440ba172df5
JM Bae <jungman82@gmail.com>
2023-07-27 09:37:17 +0900

    이메일 연동, 이메일 폼 추가

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -118,3 +118,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jungman82@gmail.com'
+EMAIL_HOST_PASSWORD = 'wrakgbytyixxpgyi'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
