2d2e919064c1f8aa989dc65139ec88231a08dcfb
JM Bae <jungman82@gmail.com>
2023-07-27 11:04:42 +0900

    url 경로 수정

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:post_id>', views.post_detail, name='post_detail'),
-    path('<int:post_id>/share', views.post_share, name='post_share'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
