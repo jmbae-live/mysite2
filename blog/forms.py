173f3aaca24ee75cf1821728107b428b5303bab8
JM Bae <jungman82@gmail.com>
2023-07-27 11:42:54 +0900

    CommentForm  클래스 생성

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 
+from blog.models import Comment
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class': 'form-control'}))
     to = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     comments = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
