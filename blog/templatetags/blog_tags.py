48024739f21b67a0c28b5d59763d542a7c3901b8
JM Bae <jungman82@gmail.com>
2023-07-27 15:44:20 +0900

    최신글 목록 출력 inclusion_tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -11,3 +11,10 @@ def get_most_commented_posts(count=5):
     return Post.objects.filter(status=Post.Status.PUBLISHED) \
                .annotate(total_comments=Count('comments')) \
                .order_by('-total_comments')[:count]
+
+
+@register.inclusion_tag('blog/post/includes/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.filter(status=Post.Status.PUBLISHED) \
+                       .order_by('-published')[:count]
+    return {'latest_posts': latest_posts}
