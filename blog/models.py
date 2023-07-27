ab77af0dc32893a21247fcc253fcd2a697ebd87d
JM Bae <jungman82@gmail.com>
2023-07-27 11:36:22 +0900

    Comment 모델 클래스 생성, 마이그레이션 추가 & 실행

--- blog/models.py
+++ blog/models.py
@@ -29,3 +29,22 @@ class Post(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('blog:post_detail', args=[self.id])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
