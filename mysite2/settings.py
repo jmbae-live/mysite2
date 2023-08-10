e030cb914525f5d136ecd7c4d5eddb22c42a0637
JM Bae <jungman82@gmail.com>
2023-08-10 13:32:43 +0900

    shop 앱 추가

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "shop.apps.ShopConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
