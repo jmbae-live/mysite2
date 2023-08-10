09c1947b3171c596fbec5de45f685adf68d26a3e
JM Bae <jungman82@gmail.com>
2023-08-10 14:30:52 +0900

    Bootstrap 스타일 정리

--- blog/forms.py
+++ blog/forms.py
@@ -13,3 +13,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'})
+        }
