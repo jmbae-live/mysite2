309f9585bc52833715af61b0c5aa22cd0665db1e
JM Bae <jungman82@gmail.com>
2023-08-11 09:27:12 +0900

    Comment

--- shop/views.py
+++ shop/views.py
@@ -6,11 +6,13 @@ from shop.models import Product
 # Create your views here.
 def add_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
+    # 세션 'cart' 정보 가져오기
     cart = request.session.get('cart', {})
     cart[str(product_id)] = {
         'quantity': 1,
         'price': str(product.price),
     }
+    # cart 변수를 세션에 저장
     request.session['cart'] = cart
     request.session.modified = True
 
