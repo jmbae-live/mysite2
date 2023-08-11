027f8f5d0c9f044afc6c02f637730301d913a50d
JM Bae <jungman82@gmail.com>
2023-08-11 10:30:37 +0900

    장바구니 삭제, 업데이트 뷰로직 만들기

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.shortcuts import render, redirect, get_object_or_404
 
 from shop.models import Product
@@ -19,5 +21,33 @@ def add_cart(request, product_id):
     return redirect('shop:cart_detail')
 
 
+def cart_remove(request, product_id):
+    cart = request.session.get('cart', {})
+    del cart[str(product_id)]
+    request.session['cart'] = cart
+    request.session.modified = True
+    return redirect('shop:cart_detail')
+
+
+def cart_update(request, product_id):
+    cart = request.session.get('cart', {})
+    cart[str(product_id)]['quantity'] = Decimal(request.POST.get('quantity'))
+    request.session['cart'] = cart
+    request.session.modified = True
+    return redirect('shop:cart_detail')
+
+
 def cart_detail(request):
-    return render(request, 'cart/detail.html', {})
+    cart = request.session.get('cart', {})
+    product_ids = cart.keys()
+    products = Product.objects.filter(id__in=product_ids)
+    for product in products:
+        cart[str(product.id)]['product'] = product
+    for key, item in cart.items():
+        item['price'] = Decimal(item['price'])
+        item['total_price'] = item['price'] * item['quantity']
+        cart[key] = item
+
+    return render(request, 'cart/detail.html', {
+        'cart_dict': cart
+    })
