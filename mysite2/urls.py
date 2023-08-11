3a03fce6a1bba39afae8155cf3e3598b33614b8b
JM Bae <jungman82@gmail.com>
2023-08-11 09:06:50 +0900

    home 페이지 url 설정

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from blog import views
+
 urlpatterns = [
+                  path("", views.post_list, name='home'),
                   path("admin/", admin.site.urls),
                   path("blog/", include('blog.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
