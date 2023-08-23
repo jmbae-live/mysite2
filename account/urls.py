faf9e58d7a140e2b304160f3dfb81e8f63d84f73
JM Bae <jungman82@gmail.com>
2023-08-23 15:05:25 +0900

    회원가입 클래스뷰 추가, 기본 템플릿 파일 추가

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from account.views import CustomLoginView
+
+from .views import RegisterView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 # Create your views here.
@@ -8,3 +11,9 @@ class CustomLoginView(LoginView):
         if request.user.is_authenticated:
             return redirect('dashboard')
         return super().dispatch(request, *args, **kwargs)
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
