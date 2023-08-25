a0cffb1b8a7dd12a436d2bd6a085bd4fbfefc9d8
JM Bae <jungman82@gmail.com>
2023-08-25 11:08:32 +0900

    결제 정보 업데이트 실습

--- shop/tasks.py
+++ shop/tasks.py
@@ -1,6 +1,10 @@
+import base64
+
+import requests
 from celery import shared_task
 from django.core.mail import send_mail
 
+from mysite2 import settings
 from shop.models import Order
 
 
@@ -18,8 +22,24 @@ def order_created(order_id):
 
 @shared_task
 def toss_payment_confirm(payment_key, order_id):
-    print('toss_payment_confirm:' + payment_key)
-    # 토스 인증
+    # 토스 인증 - 시크릿 키를 base64 로 인코딩해서 헤더로 전달함
+    encoding_str = (settings.TOSS_SECRET_KEY + ':').encode()
+    encoded_secret_key = base64.urlsafe_b64encode(encoding_str)
+    print(str(encoded_secret_key))
     # 결제 확인 API 호출
+    toss_api_url = 'https://api.tosspayments.com/v1/payments/' + payment_key
+    headers = {
+        'Authorization': 'Basic ' + encoded_secret_key.decode('utf-8')
+    }
     # 결과 확인 데이터베이스 업데이트
+    response = requests.get(toss_api_url, headers=headers)
+    res_data = dict(response.json())
+    order_id = int(res_data['orderId'].replace('order-', ''))
+    order = Order.objects.get(id=order_id)
+    if res_data['status'] == 'DONE':
+        order.paid = True
+        order.save()
+    else:
+        print('status:' + res_data['status'])
+    # print(dict(response.json()))
     return
